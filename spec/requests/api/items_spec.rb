49a884388a5717f0cb896901b270e262af4b8596
Vassiliki Karagianniki <78561945+vassilikikrg@users.noreply.github.com>
2024-01-10 16:05:25 +0200

    Swagger Setup
    HTTP Requests' body specidfied

--- spec/requests/api/items_spec.rb
+++ spec/requests/api/items_spec.rb
@@ -1,4 +1,58 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/items', type: :request do
+  path '/todos/{todo_id}/items' do
+    get 'Retrieves all items of a todo list' do
+      produces 'application/json'
+      parameter name: :todo_id, in: :path, type: :integer
+
+      response '200', 'items found' do
+        run_test!
+      end
+    end
+
+    post 'Creates an item for a todo list' do
+      consumes 'application/json'
+      parameter name: :todo_id, in: :path, type: :integer
+      parameter name: :item, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          done: { type: :boolean },
+        },
+        required: ['name', 'done'],
+      }
+
+      response '201', 'item created' do
+        run_test!
+      end
+    end
+  end
+
+  path '/todos/{todo_id}/items/{id}' do
+    put 'Updates an item of a todo list' do
+      consumes 'application/json'
+      parameter name: :todo_id, in: :path, type: :integer
+      parameter name: :id, in: :path, type: :integer
+      parameter name: :item, in: :body, schema: {
+        type: :object,
+        properties: {
+          done: { type: :boolean },
+        },
+      }
+
+      response '204', 'item updated' do
+        run_test!
+      end
+    end
+
+    delete 'Deletes an item of a todo list' do
+      parameter name: :todo_id, in: :path, type: :integer
+      parameter name: :id, in: :path, type: :integer
+
+      response '204', 'item deleted' do
+        run_test!
+      end
+    end
+  end
 end
--- spec/requests/api/todos_spec.rb
+++ spec/requests/api/todos_spec.rb
@@ -1,4 +1,54 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/todos', type: :request do
+  path '/todos' do
+    get 'Retrieves all todo lists' do
+      produces 'application/json'
+      response '200', 'todo lists found' do
+        run_test!
+      end
+    end
+
+    post 'Creates a todo list' do
+      consumes 'application/json'
+      parameter name: :todo, in: :body, schema: {
+        type: :object,
+        properties: {
+          title: { type: :string },
+          created_by: { type: :integer },
+        },
+        required: ['title', 'created_by'],
+      }
+
+      response '201', 'todo list created' do
+        run_test!
+      end
+    end
+  end
+
+  path '/todos/{id}' do
+    put 'Updates a todo list' do
+      consumes 'application/json'
+      parameter name: :id, in: :path, type: :integer
+      parameter name: :todo, in: :body, schema: {
+        type: :object,
+        properties: {
+          title: { type: :string },
+        },
+      }
+
+      response '204', 'todo list updated' do
+        run_test!
+      end
+    end
+
+    delete 'Deletes a todo list' do
+      parameter name: :id, in: :path, type: :integer
+
+      response '204', 'todo list deleted' do
+        run_test!
+      end
+    end
+  end
 end
+
